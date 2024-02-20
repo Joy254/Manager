f379b51bf6cdf97fb2b99bec86fcab6a8b5b6667
JOY254 <nyorgijoy@gmail.com>
2024-02-20 00:39:16 -0600

    import comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
 
-function Comment() {
+const Comment = ({ comment, editingCommentId, handleEditComment, handleSaveComment, handleDeleteComment }) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <ListGroup.Item key={comment.id} className="d-flex justify-content-between">
+      <div>
+        {editingCommentId === comment.id ? (
+          <>
+            <input
+              type="text"
+              value={comment.text}
+              onChange={(e) => handleSaveComment(comment.id, e.target.value)}
+            />
+            <Button
+              variant="warning"
+              className="ms-2"
+              onClick={() => handleSaveComment(comment.id, comment.text)}
+            >
+              Save
+            </Button>
+          </>
+        ) : (
+          <>
+            {comment.text}
+            <Button
+              variant="info"
+              className="ms-2"
+              onClick={() => handleEditComment(comment.id)}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="danger"
+              className="ms-2"
+              onClick={() => handleDeleteComment(comment.id)}
+            >
+              Delete
+            </Button>
+          </>
+        )}
+      </div>
+    </ListGroup.Item>
+  );
 }
 
-export default Comment
+export default Comment;
